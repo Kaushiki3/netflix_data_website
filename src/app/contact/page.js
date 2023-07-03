04a14d737d085525cb2b25dee1413919f59f3329
Kaushiki3 <133948921+Kaushiki3@users.noreply.github.com>
2023-07-03 14:19:47 +0530

    Update page.js
    just removed a comma

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -9,7 +9,7 @@ const Contact = () => {
        <h1>contact us</h1>
        <Contactcard/>
        <section className={styles.contact_section}>
-        <h2>We'd love to hear<span> from you</span> </h2>
+        <h2>We love to hear<span> from you</span> </h2>
         <ContactForm />
        </section>
        </div>
