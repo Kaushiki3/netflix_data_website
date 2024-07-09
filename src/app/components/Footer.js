f5a213ce3294e8ef54e58e63eb47af835674259c
Kaushiki Nanda Duarah <133948921+Kaushiki3@users.noreply.github.com>
2024-07-09 19:09:37 +0530

    Update Footer.js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -22,7 +22,7 @@ const Footer = () => {
                         <div className={footerStyles[`media-icons`]}>
                             <Link href="#"><i > <FaFacebookF/> </i></Link>
                             <Link href="#"><i > <FaTwitter/> </i> </Link>
-                            <Link href="https://www.instagram.com/thapatechnical/" target="_blank"><i > <FaInstagram/>  </i></Link>
+                            <Link href="#" target="_blank"><i > <FaInstagram/>  </i></Link>
                             <Link href="#"><i > <FaLinkedinIn /> </i></Link>
                             <Link href="#"><i > <FaYoutube />  </i></Link>
                         </div>
@@ -66,7 +66,7 @@ const Footer = () => {
                 <div className={footerStyles['bottom-details']}>
                     <div className={footerStyles.bottom_text}>
                         <span className={footerStyles.copyright_text}> Copyright © 2023
-                            <Link href="/>"> Thapa Technical.</Link> All rights reserved </span>
+                            <Link href="/>"> Saya</Link> All rights reserved </span>
                         <span className={footerStyles.policy_terms}>
                           <Link href="/">Privacy policy</Link>
                           <Link href="/">Terms & condition</Link>
